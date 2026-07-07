4df98851e6a74f0efc2ec224986eff7cf77d650c
Akshat Sohani <akshatsohani2005@gmail.com>
2026-07-07 09:36:01 +0530

    Fix build errors for Vercel deployment

--- src/app/api/student/garbage/route.js
+++ src/app/api/student/garbage/route.js
@@ -1,23 +1,12 @@
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
-import { getcurruser } from "@/lib/auth";
-import { unauthorized } from "next/navigation";
+
 
 export async function POST(req) {
     try{ const formData = await req.formData();
         const field1 = formData.get("field1");
         const field2 = formData.get("field2");
         const field3 = formData.get("field3");
-       
-        const student = await getcurruser();
-
-        if(!student){
-            return NextResponse.json({unauthorized},
-                {status:401}
-            );
-        }
-
-        const date = new Date().toString();
         
        const complain = await prisma.Garbage.create({
             data:{
