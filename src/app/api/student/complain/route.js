58d9c594925b45df73ea0be3da9ce3fc104ea872
Akshat Sohani <akshatsohani2005@gmail.com>
2026-07-10 00:58:26 +0530

    working on complain page

--- src/app/api/student/complain/route.js
+++ src/app/api/student/complain/route.js
@@ -5,30 +5,30 @@ import { NextResponse } from "next/server";
 
 
 export async function POST() {
-    try {
+    // try {
      const data = await getcurruser();
      console.log(data);
      const id = data.id
-    
+     return NextResponse(data);
 
-        const Garbage = await prisma.Garbage.findMany({
-            where:{
-                name : id,
-            }
-        })
-        if (Garbage.length === 0) {
-    return NextResponse.json({
-        Garbage: [],
-        msg : "You have not made any complaints"
-    });
-}
-    return NextResponse.json({Garbage: Garbage,
-        msg: "Complaints fetched Successfully"
-    })
-     } catch (error) {
-    //  return NextResponse.json( { message: "Session cookie not found" },
-    // { status: 401 })    
-     return NextResponse.json( { message: "Session cookie not found" },
-    { status: 401 })    
-    }
-}
+//         const Garbage = await prisma.Garbage.findMany({
+//             where:{
+//                 name : id,
+//             }
+//         })
+//         if (Garbage.length === 0) {
+//     return NextResponse.json({
+//         Garbage: [],
+//         msg : "You have not made any complaints"
+//     });
+// }
+//     return NextResponse.json({Garbage: Garbage,
+//         msg: "Complaints fetched Successfully"
+//     })
+//      } catch (error) {
+//     //  return NextResponse.json( { message: "Session cookie not found" },
+//     // { status: 401 })    
+//      return NextResponse.json( { message: "Session cookie not found" },
+//     { status: 401 })    
+//     }
+   }
